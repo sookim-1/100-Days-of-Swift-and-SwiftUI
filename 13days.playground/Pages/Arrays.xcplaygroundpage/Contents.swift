07a94940c4d2763d06759f0fe97dfb352b66fd37
Sookim18 <scstnghks@naver.com>
2021-06-21 01:25:13 +0900

    feat : 13단원 추가

---
+++ 13days.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var evenNumbers = [2, 4, 6, 8]
+var songs = ["Shake it Off", "You Belong with Me", "Back to December"]
+
+songs[0]
+songs[1]
+songs[2]
+type(of: songs)
+//: [Next](@next)
---
+++ 13days.playground/Pages/String interpolation .xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var name = "Tim McGraw"
+var age = 25
+var latitude = 36.166667
+
+"Your name is \(name), your age is \(age), and your latitude is \(latitude)"
+"You are \(age) years old. In another \(age) years you will be \(age * 2)."
+
+
+//: [Next](@next)
