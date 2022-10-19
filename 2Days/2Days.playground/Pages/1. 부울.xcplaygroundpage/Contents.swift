1fb6f87953c976418987cc0c8990d35b82c7ff78
sookim-1 <scstnghks@naver.com>
2022-10-19 22:48:55 +0900

    [Feat] : 2Days 플레이그라운드 예제 및 정리글 추가

---
+++ 2Days/2Days.playground/Pages/1. 부울.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+//: 부울은 true, false 둘 중 하나의 값만 가집니다.
+let goodDogs = true
+let gameOver = false
+
+//: !연산자
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+isAuthenticated = !isAuthenticated
+
+//: toggle메서드
+var gameStart = false
+gameStart.toggle()
