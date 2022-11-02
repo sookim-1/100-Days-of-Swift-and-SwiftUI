c1be7aa51bfe2a4f6e3bc79a8584d3f1f3b1030a
sookim-1 <scstnghks@naver.com>
2022-11-03 00:11:47 +0900

    [Feat] : 16Days 프로젝트1-WeSplit 예제 및 정리글 추가

---
+++ Days16/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 수킴 on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
