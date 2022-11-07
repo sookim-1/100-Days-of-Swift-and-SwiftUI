613be96e98a8ea37e27c5bb1f467ce92094cdc4c
sookim-1 <scstnghks@gmail.com>
2022-11-08 00:43:36 +0900

    [Feat] : 21Days 프로젝트-GuessTheFlag 예제 및 정리글 추가

---
+++ Days21/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by 수킴 on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
