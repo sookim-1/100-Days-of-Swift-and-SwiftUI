3671b52a98938a1890d78426d372715010cf653d
sookim-1 <scstnghks@gmail.com>
2022-12-05 23:28:32 +0900

    [Feat] : 49Days CupcakeCorner 예제 및 정리글 추가

---
+++ Days49/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by 수킴 on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
